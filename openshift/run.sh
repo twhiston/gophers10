1cae8950fdcbe96f2c2ae9cb720df3263d8f74ac
Tom Whiston <tom.whiston@gmail.com>
2018-03-02 16:04:10 +0100

    add presentation

--- openshift/run.sh
+++ openshift/run.sh
@@ -2,6 +2,6 @@
 
 oc project tomw-gophers-production
 oc new-app https://github.com/twhiston/gophers10 --name=gophers --strategy=docker --context-dir=gophers10
-oc expose svc/gophersoc get triggers
+oc expose svc/gophers
 
 oc get bc
